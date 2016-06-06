316be01c02f672c264bdf4cfc2d4d377b319d20b
unknown <asatpathy@EMETRIC>
2016-06-06 15:40:55 -0500

    removed col4

--- Security/Users/testlogin.sql
+++ Security/Users/testlogin.sql
@@ -1,5 +1,3 @@
 IF NOT EXISTS (SELECT * FROM master.dbo.syslogins WHERE loginname = N'testlogin')
 CREATE LOGIN [testlogin] WITH PASSWORD = 'p@ssw0rd'
 GO
-CREATE USER [testlogin] FOR LOGIN [testlogin]
-GO
